3f51c765f8ed69acf98b8f738bb83e759137d403
Mojmir Fendek <mfendek@silverstripe.com>
2020-01-28 12:35:57 +1300

    Save action logic improved

--- src/Handler/Form/SaveHandler.php
+++ src/Handler/Form/SaveHandler.php
@@ -46,10 +46,74 @@ class SaveHandler extends Handler
             return parent::createSnapshot($context);
         }
 
-        if ($record->isModifiedOnDraft()) {
+        if ($this->hasNewVersion($record)) {
             return parent::createSnapshot($context);
         }
 
         return null;
+    }
+
+    /**
+     * This logic tries to determine if we need to create a snapshot or not based on the action
+     * if save action had no impact, there is no reason to create a snapshot for such action
+     *
+     * @param DataObject|Versioned $record
+     * @return bool
+     */
+    protected function hasNewVersion(DataObject $record): bool
+    {
+        // in case draft is the same as live - no need to create snapshot
+        if (!$record->isModifiedOnDraft()) {
+            return false;
+        }
+
+        // attempt to find the most recent snapshot where the record acts as the origin
+        /** @var Snapshot $snapshot */
+        $snapshot = Snapshot::get()
+            ->filter([
+                'OriginID' => $record->ID,
+                'OriginClass' => $record->baseClass(),
+            ])
+            ->sort([
+                'LastEdited' => 'DESC',
+                'ID' => 'DESC',
+            ])
+            ->first();
+
+        // no such snapshot found - we need to create a snapshot
+        if ($snapshot === null) {
+            return true;
+        }
+
+        // attempt to find snapshot item which represents the origin
+        $version = 0;
+        foreach ($snapshot->Items() as $item) {
+            if ((int) $item->ObjectID !== (int) $record->ID) {
+                continue;
+            }
+
+            if ($item->ObjectClass !== $record->baseClass()) {
+                continue;
+            }
+
+            if ($version > $item->Version) {
+                continue;
+            }
+
+            $version = $item->Version;
+        }
+
+        // no version was found for the record in the snapshot history - we need to create a snapshot
+        if ($version === 0) {
+            return true;
+        }
+
+        // record has a newer version compated to snapshot - we need to create a snapshot
+        if ($record->Version > $version) {
+            return true;
+        }
+
+        // record doesn't have a new version - no need to create snapshot
+        return false;
     }
 }
